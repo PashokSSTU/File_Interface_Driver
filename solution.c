285546934a1941c9eac1b83724a45201ff05cba0
Pavel khromov <hromov.pavel19.20@gmail.com>
2023-01-08 23:36:45 +0700

    Change chrdev registration functions

--- solution.c
+++ solution.c
@@ -9,8 +9,10 @@
 
 MODULE_LICENSE("GPL");
 
-#define DEVICE_NAME "solution_node"
-#define DEVICE_MAJOR 240
+#define DEVICE_NAME     "solution_node"
+#define DEVICE_MAJOR    510
+#define FIRST_MINOR     0
+#define MAX_MINORS      1
 
 /*Interface functions prototypes*/
 static ssize_t read_interface (struct file *, char *, size_t, loff_t *);
@@ -18,34 +20,58 @@ static ssize_t write_interface (struct file *, const char *, size_t, loff_t *);
 static int open_interface (struct inode *, struct file *);
 static int release_interface (struct inode *, struct file *);
 
-/*Struct of pointers to interface functions*/
+/*Global variables*/
+int a, b, sum;
+char a_buffer[10], b_buffer[10];
+
+struct my_device_data 
+{
+    struct cdev cdev;
+};
+
+static struct my_device_data devs[MAX_MINORS];
+
 static struct file_operations fops = {
+    .owner = THIS_MODULE,
     .read = read_interface,
     .write = write_interface,
     .open = open_interface,
     .release = release_interface
 };
 
-/*Global variables*/
-int a, b, sum;
-char a_buffer[10], b_buffer[10];
+static int minor_count = 1;
 
 static int __init init_solution(void)
 {
-    int result = register_chrdev(DEVICE_MAJOR, DEVICE_NAME, &fops);
-    if(result < 0)
+    printk(KERN_INFO "Initialization of chrdev module started.\n");
+    int err = register_chrdev_region(MKDEV(DEVICE_MAJOR, FIRST_MINOR), minor_count, DEVICE_NAME);
+    if(err != 0)
     {
-        printk(KERN_ALERT "Init file_interface_driver module.\n");
-        return result;
+        printk(KERN_ALERT "Error of chrdev registration!\n");
+        return err;
     }
-    
+    printk(KERN_INFO "Initialization of chrdev module ended.\n");
+
+    int i;
+    for(i = 0; i < MAX_MINORS; i++)
+    {
+        cdev_init(&devs[i].cdev, &fops);
+        cdev_add(&devs[i].cdev, MKDEV(DEVICE_MAJOR, i), 1);
+    }
+
     return 0;
 }
 
 static void __exit cleanup_solution(void)
 {
-    unregister_chrdev(DEVICE_MAJOR, DEVICE_NAME);
-    printk(KERN_INFO "Cleanup file_interface_driver module.\n");
+    int i;
+    for(i = 0; i < MAX_MINORS; i++)
+    {
+        cdev_del(&devs[i].cdev);
+    }
+
+    unregister_chrdev_region(MKDEV(DEVICE_MAJOR, 0), minor_count);
+    printk(KERN_INFO "Cleanup chrdev module.\n");
 }
 
 module_init (init_solution);
